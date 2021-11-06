76e9c36d032825a20a7570f469b8dd197e710aa1
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-06 20:00:59 +0100

    addition

---
+++ 0x00-math_complex/4-addition.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+/**
+ * addition - performs the addition operation to complex numbers.
+ * @c1: complex number of type struct complex.
+ * @c2: complex number of type struct complex.
+ * @c3: points to a struct complex.
+ * Return: c3.
+ */
+void addition(complex c1, complex c2, complex *c3)
+{
+	c3->re = c1.re + c2.re;
+	c3->im = c1.im + c2.im;
+}
