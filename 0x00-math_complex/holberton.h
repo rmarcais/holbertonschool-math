fcc4058f15d7841791a6a3ee888f660471799c51
Rémi Marçais <remi.marcais87@gmail.com>
2021-11-07 14:10:34 +0100

    multiplication

--- 0x00-math_complex/holberton.h
+++ 0x00-math_complex/holberton.h
@@ -18,4 +18,5 @@ double modulus(complex c);
 double argument(complex c);
 void addition(complex c1, complex c2, complex *c3);
 void substraction(complex c1, complex c2, complex *c3);
+void multiplication(complex c1, complex c2, complex *c3);
 #endif
